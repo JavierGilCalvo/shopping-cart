7fab4f46a18fde1ef0509198e33514ec7c1f358c
13JaviG <13javiergc@gmail.com>
2023-02-17 20:46:38 +0100

    Filter by price and filter by category

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products'
+import { useState } from 'react'
+import { Header } from './components/Header'
 
 function App () {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
     <>
-      <h1>Shopping Cart</h1>
-      <Products products={products} />
+      <Header changeFilters={setFilters} />
+      <Products products={filteredProducts} />
     </>
   )
 }
