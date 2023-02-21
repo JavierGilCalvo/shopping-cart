7bd8772bdc606fe4074400cab0bc46fbd2bb69ef
13JaviG <13javiergc@gmail.com>
2023-02-21 13:55:19 +0100

    local storage cart state

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -5,7 +5,12 @@ export const CART_ACTION_TYPES = {
   CLEAR_CART: 'CLEAR_CART'
 }
 
-export const initialCartState = []
+export const initialCartState = JSON.parse(window.localStorage.getItem('cart')) || []
+
+const updateLocalStorage = state => {
+  window.localStorage.setItem('cart', JSON.stringify(state))
+}
+
 export const cartReducer = (state, action) => {
   const { type: actionType, payload: actionPayload } = action
   const { id } = actionPayload
@@ -17,29 +22,36 @@ export const cartReducer = (state, action) => {
       if (productInCartIndex >= 0) {
         const newCart = structuredClone(state)
         newCart[productInCartIndex].quantity += 1
+        updateLocalStorage(newCart)
         return newCart
       }
 
       // The product is not in the cart
-      return [
+      const newCart = [
         ...state,
         {
           ...actionPayload,
           quantity: 1
         }
       ]
+      updateLocalStorage(newCart)
+      return newCart
     }
     case CART_ACTION_TYPES.REMOVE_FROM_CART: {
-      return state.filter(item => item.id !== id)
+      const newCart = state.filter(item => item.id !== id)
+      updateLocalStorage(newCart)
+      return newCart
     }
     case CART_ACTION_TYPES.EXTRACT_ONE_FROM_CART: {
       const productInCartIndex = state.findIndex(item => item.id === id)
       const newCart = structuredClone(state)
       newCart[productInCartIndex].quantity -= 1
       const checkIfRemovable = newCart[productInCartIndex].quantity === 0
-      return checkIfRemovable
+      const cartToUpdate = checkIfRemovable
         ? state.filter(item => item.id !== id)
         : newCart
+      updateLocalStorage(cartToUpdate)
+      return cartToUpdate
     }
     default:{
       return []
